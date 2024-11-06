d6845caa3dde64bcd0bde611ea4dc1b181c75f7a
Aina <113420341+ainaorozcogonzalez@users.noreply.github.com>
2024-11-06 17:50:11 +0100

    Update estructura.sql

--- BD/estructura.sql
+++ BD/estructura.sql
@@ -67,3 +67,72 @@ INSERT INTO Mesas (id_sala, numero, capacidad) VALUES
 (6, 1, 2),
 (7, 1, 2),
 (8, 1, 2);
+
+
+
+
+CREATE SCHEMA `db_restaurante` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+USE db_restaurante;
+
+CREATE TABLE `db_restaurante`.`usuarios` (
+  `id_usuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre_completo` VARCHAR(200) NOT NULL,
+  `contraseña` VARCHAR(255) NOT NULL,
+  `tipo_usuario` ENUM('camarero', 'manager') NOT NULL,
+  PRIMARY KEY (`id_usuario`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+CREATE TABLE `db_restaurante`.`salas` (
+  `id_sala` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(100) NOT NULL,
+  `capacidad` INT NOT NULL,
+  PRIMARY KEY (`id_sala`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+
+CREATE TABLE `db_restaurante`.`mesas` (
+  `id_mesa` INT NOT NULL AUTO_INCREMENT,
+  `capacidad` INT NOT NULL,
+  `estado` VARCHAR(45) NULL,
+  `Mesascol` ENUM('libre', 'ocupada') NULL DEFAULT 'libre',
+  `id_sala` INT NULL,
+  PRIMARY KEY (`id_mesa`),
+  INDEX `id_sala_idx` (`id_sala` ASC) VISIBLE,
+  CONSTRAINT `id_sala`
+    FOREIGN KEY (`id_sala`)
+    REFERENCES `db_restaurante`.`salas` (`id_sala`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
+
+
+CREATE TABLE `db_restaurante`.`ocupaciones` (
+  `id_ocupacion` INT NOT NULL AUTO_INCREMENT,
+  `id_mesa` INT NULL,
+  `id_usuario` INT NULL,
+  `fecha_ocupacion` DATETIME NOT NULL,
+  `fecha_libera` DATETIME NULL,
+  PRIMARY KEY (`id_ocupacion`),
+  INDEX `id_mesa_idx` (`id_mesa` ASC) VISIBLE,
+  INDEX `id_usuario_idx` (`id_usuario` ASC) VISIBLE,
+  CONSTRAINT `id_mesa`
+    FOREIGN KEY (`id_mesa`)
+    REFERENCES `db_restaurante`.`mesas` (`id_mesa`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_usuario`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `db_restaurante`.`usuarios` (`id_usuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_unicode_ci;
