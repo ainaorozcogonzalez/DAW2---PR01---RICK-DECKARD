3c5a4b4943a2d832a28a6c8c172d23e9ca7d3160
dbuenaño <deibycesar10@gmail.com>
2024-11-06 19:32:26 +0100

    actualizacion de index

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
 $host = "localhost";
 $usuario = "root";
-$contrasena = "";
+$contrasena = "Agustin51";
 $nombre_bd = "db_restaurante";
 $con = mysqli_connect($host, $usuario, $contrasena, $nombre_bd);
 
@@ -10,15 +10,6 @@ if (mysqli_connect_errno()) {
     die("Error al conectar a la base de datos: " . mysqli_connect_error());
 }
 
-try {
-
-    $conn = mysqli_connect($host, $usuario,$contrasena, $nombre_bd);
-    echo "conectado con el servidor <br>";
-}
-catch (Exception $e) {
-    echo "Error de conexión: ". $e->getMessage();
-    die();
-}
 
 
 
