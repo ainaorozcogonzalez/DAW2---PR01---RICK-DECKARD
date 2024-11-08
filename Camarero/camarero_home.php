1debaa1a2d1555a36640d976cd7ad51bbd921ebd
dbuenaño <deibycesar10@gmail.com>
2024-11-08 17:46:26 +0100

    rutas puestas

--- Camarero/camarero_home.php
+++ Camarero/camarero_home.php
@@ -1,203 +1,124 @@
-<?php
-    include_once("../conexion.php");
-    session_start();
-
-    if (!isset($_SESSION['id_usuario'])) {
-
-        header('Location: ' . '../index.php');
-        exit();
-
-    } else {
-
-        $camareroActual = mysqli_real_escape_string($con, htmlspecialchars($_SESSION['id_usuario']));
-
-        // SELECCIONAMOS todo DE 'mesas', JUNTAMOS con salas EN la id_sala,
-        // JUNTAMOS (LEFT JOIN) 'ocupaciones' EN la id_mesa Y en el caso que la fecha de liberación SEA NULA, O sea mayor que la fecha actual (la mesa sigue ocupada, o sigue ocupada pero se liberará en una fecha futura a la de ahora)
-        // JUNTAMOS (LEFT JOIN) 'usuarios' EN la id_usuario
-        // CUANDO el camarero de la ocupacion sea el mismo que el usuario de la sesión, o el camarero SEA NULO (las mesas que están ocupadas y las que no)
-        $sqlPáginaCamareros = "SELECT mesas.id_mesa, mesas.capacidad, mesas.estado, mesas.id_sala,
-        salas.id_sala, salas.nombre, salas.capacidad,
-        ocupaciones.id_ocupacion, ocupaciones.id_mesa, ocupaciones.fecha_ocupacion, ocupaciones.fecha_libera,
-        usuarios.id_usuario, usuarios.nombre_completo, usuarios.contraseña, usuarios.tipo_usuario
-        FROM mesas
-        INNER JOIN salas ON salas.id_sala = mesas.id_sala
-        LEFT JOIN ocupaciones ON ocupaciones.id_mesa = mesas.id_mesa AND (ocupaciones.fecha_libera IS NULL OR ocupaciones.fecha_libera > NOW())
-        LEFT JOIN usuarios ON usuarios.id_usuario = ocupaciones.id_usuario
-        WHERE ocupaciones.id_usuario = ? OR ocupaciones.id_usuario IS NULL
-        ORDER BY salas.id_sala, mesas.id_mesa";
-?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Header con Menú Desplegable</title>
-    <!-- Bootstrap CSS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Bootstrap Bundle JS (con Popper) -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
-
-    <style>
-        /* Estilo para el contenedor de la imagen */
-        .image-container {
-            text-align: center;
-            margin-top: 20px;
-        }
-        .image-container img {
-            max-width: 100%;
-            height: auto;
-        }
-    </style>
+    <title>Manager</title>
+    <link rel="stylesheet" href="../CSS/styles.css">
 </head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">LOGO</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarContent">
-                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <!-- Menú Terrazas -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="terrazasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Terrazas
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="terrazasDropdown">
-                            <?php
-                                
-                                $sqlTerrazas = "SELECT * FROM salas WHERE id_sala <= 3";
-                                $stmtTerrazas = mysqli_prepare($con, $sqlTerrazas);
-                                mysqli_stmt_execute($stmtTerrazas);
-                                mysqli_stmt_store_result($stmtTerrazas);
 
-                                if (mysqli_stmt_num_rows($stmtTerrazas) > 0) {
-
-                                    mysqli_stmt_bind_result($stmtTerrazas, $id_sala, $nombre, $capacidad);
-
-                                    while (mysqli_stmt_fetch($stmtTerrazas)) {
-                                        echo '<li><a class="dropdown-item" href="#" onclick="showImage(\'../img/' . htmlspecialchars($nombre) . '.jpg\')">' . htmlspecialchars($nombre) . '</a></li>';
-                                    }
-
-                                }
-                                
-                            ?>
-                        </ul>
-                    </li>
-                    <!-- Menú Comedores -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="comedoresDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Comedores
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="comedoresDropdown">
-                            <?php
-                                
-                                $sqlTerrazas = "SELECT * FROM salas WHERE id_sala BETWEEN 4 AND 5";
-                                $stmtTerrazas = mysqli_prepare($con, $sqlTerrazas);
-                                mysqli_stmt_execute($stmtTerrazas);
-                                mysqli_stmt_store_result($stmtTerrazas);
-
-                                if (mysqli_stmt_num_rows($stmtTerrazas) > 0) {
-
-                                    mysqli_stmt_bind_result($stmtTerrazas, $id_sala, $nombre, $capacidad);
-
-                                    while (mysqli_stmt_fetch($stmtTerrazas)) {
-                                        echo '<li><a class="dropdown-item" href="#" onclick="showImage(\'../img/' . htmlspecialchars($nombre) . '.jpg\')">' . htmlspecialchars($nombre) . '</a></li>';
-                                    }
-
-                                }
-                                
-                            ?>
-                        </ul>
-                    </li>
-                    <!-- Menú Salas Privadas -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="salasPrivadasDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Salas Privadas
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="salasPrivadasDropdown">
-                            <?php
-                                
-                                $sqlTerrazas = "SELECT * FROM salas WHERE id_sala BETWEEN 6 AND 8";
-                                $stmtTerrazas = mysqli_prepare($con, $sqlTerrazas);
-                                mysqli_stmt_execute($stmtTerrazas);
-                                mysqli_stmt_store_result($stmtTerrazas);
-
-                                if (mysqli_stmt_num_rows($stmtTerrazas) > 0) {
-
-                                    mysqli_stmt_bind_result($stmtTerrazas, $id_sala, $nombre, $capacidad);
-
-                                    while (mysqli_stmt_fetch($stmtTerrazas)) {
-                                        echo '<li><a class="dropdown-item" href="#" onclick="showImage(\'../img/' . htmlspecialchars($nombre) . '.jpg\')">' . htmlspecialchars($nombre) . '</a></li>';
-                                    }
-
-                                }
-                                
-                            ?>
-                        </ul>
-                    </li>
-                </ul>
+<body id="bodyMan">
+    <div id="ocultarImg" class="">
+        <div class="column-1">
+            <h1 id="h1Sel">Seleccionar sala</h1>
+            <div class="image-container">
+                <a href="" id="Comedor">
+                    <h3 class="text-overlay Comedor">Comedor</h3>
+                    <img class="imgMan Comedor" src="../img/ComedorBtn.jpg" alt="Comedor">
+                </a>
+            </div>
+            <div class="image-container">
+                <a href="" id="Privada">
+                    <h3 class="text-overlay Privada">Privada</h3>
+                    <img class="imgMan Privada" src="../img/PrivadaBtn.png" alt="Privada">
+                </a>
+            </div>
+            <div class="image-container">
+                <a href="" id="Terraza">
+                    <h3 class="text-overlay Terraza">Terraza</h3>
+                    <img class="imgMan Terraza" src="../img/TerrazaBtn.png" alt="Terraza">
+                </a>
             </div>
         </div>
-    </nav>
+    </div>
+    <!-- Mostrar Comedores -->
 
-    <?php
+    <div id="Comedores" class="content">
+        <div class="column-1 flex">
+            <div>
+                <h1 id="h1Sel">Seleccionar Comedor</h1>
+                <div class="image-container">
+                    <a href="comedor1.php" id="Comedor">
+                        <h3 class="text-overlay Comedor">Comedor 1</h3>
+                        <img class="imgComedor" src="../img/Comedores.png" alt="Comedor">
+                    </a>
+                </div>
+                <div class="image-container">
+                    <a href="comedor2.php" id="Privada">
+                        <h3 class="text-overlay Privada">Comedor 2</h3>
+                        <img class="imgComedor" src="../img/Comedores.png" alt="Privada">
+                    </a>
+                </div>
+                <button id="volverComedor" class="volverBtn">Volver</button>
+            </div>
 
-        $stmtPáginaCamareros = mysqli_prepare($con, $sqlPáginaCamareros);
-        mysqli_stmt_bind_param($stmtPáginaCamareros, "i", $camareroActual);
-        mysqli_stmt_execute($stmtPáginaCamareros);
-        mysqli_stmt_store_result($stmtPáginaCamareros);
+        </div>
+    </div>
+    <!-- Mostrar Salas Privadas -->
+    <div id="Privadas" class="content">
+        <div class="column-1">
+            <h1 id="h1Sel">Seleccionar Sala Privada</h1>
+            <div class="flex">
+                <div class="image-container">
+                    <a href="privada1.php" id="Comedor">
+                        <h3 class="text-overlay Comedor">Privada 1</h3>
+                        <img class="imgSalaPriv" src="../img/SalasPrivadas.png" alt="Comedor">
+                    </a>
+                </div>
+                <div class="image-container">
+                    <a href="privada2.php" id="Privada">
+                        <h3 class="text-overlay Privada">Privada 2</h3>
+                        <img class="imgSalaPriv" src="../img/SalasPrivadas.png" alt="Privada">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="column-1 flex">
+            <div class="image-container">
+                <a href="privada3.php" id="Terraza">
+                    <h3 class="text-overlay Terraza">Privada 3</h3>
+                    <img class="imgSalaPriv" src="../img/SalasPrivadas.png" alt="Terraza">
+                </a>
+            </div>
+            <div class="image-container">
+                <a href="privada4.php" id="Terraza">
+                    <h3 class="text-overlay Terraza">Privada 4</h3>
+                    <img class="imgSalaPriv" src="../img/SalasPrivadas.png" alt="Terraza">
+                </a>
+            </div>
+        </div>
 
-        if (mysqli_stmt_num_rows($stmtPáginaCamareros) > 0) {
-
-            // Consulta a los datos de las 'mesas', 'salas', 'ocupaciones' y 'usuarios' (en orden)
-            mysqli_stmt_bind_result($stmtPáginaCamareros, $id_mesa, $capacidadMesa, $estadoMesa, $idSalaDeLaMesa,
-            $id_sala, $nombreSala, $capacidadSala,
-            $idMesaOcupada, $idCamareroQueOfreceLaMesa, $fechaOcupacion, $fechaLibera,
-            $id_usuario, $nombre, $contrasena, $tipoUsuario);
-
-
-            while (mysqli_stmt_fetch($stmtPáginaCamareros)) {
-
-                echo "<div>";
-                
-                echo "<p class=><strong>Mesa " . htmlspecialchars($id_mesa) . ":</strong> " . htmlspecialchars($estadoMesa) . "</p>";
-
-                if ($nombre != NULL) {
-                    echo "<p class=><strong>Camarero/a: </strong>" . htmlspecialchars($nombre) . "</p>";
-                } else {
-                    echo "<p class=><strong>Camarero/a: </strong> (Nadie ha asignado esta mesa) </p>";
-                }
-
-                echo "</div>";
-
-            }
-        } else {
-            echo "<p>No hay mesas</p>";
-        }
-
-    ?>
-
-    <!-- Contenedor para mostrar la imagen -->
-    <div class="image-container">
-        <img id="displayedImage" src="" alt="Selecciona una terraza" style="display: none;">
+        <button id="volverPrivada" class="volverBtn">Volver</button>
+    </div>
+    <!-- Mostrar Terrazas -->
+    <div id="Terrazas" class="content">
+        <div class="column-1">
+            <h1 id="h1Sel">Seleccionar Terraza</h1>
+            <div class="image-container">
+                <a href="terraza1.php" id="Comedor">
+                    <h3 class="text-overlay Comedor">Terraza 1</h3>
+                    <img class="imgTerraza" src="../img/Terrazas.png" alt="Comedor">
+                </a>
+            </div>
+            <div class="image-container">
+                <a href="terraza2.php" id="Privada">
+                    <h3 class="text-overlay Privada">Terraza 2</h3>
+                    <img class="imgTerraza" src="../img/Terrazas.png" alt="Privada">
+                </a>
+            </div>
+            <div class="image-container">
+                <a href="terraza3.php" id="Terraza">
+                    <h3 class="text-overlay Terraza">Terraza 3</h3>
+                    <img class="imgTerraza" src="../img/Terrazas.png" alt="Terraza">
+                </a>
+            </div>
+            <button id="volverTerraza" class="volverBtn">Volver</button>
+        </div>
     </div>
 
-    <script>
-        
-        // Función para mostrar la imagen según la terraza seleccionada
-        function showImage(imageName) {
-            const img = document.getElementById('displayedImage');
-            img.src = imageName; // Asigna el nombre de la imagen al atributo src
-            img.style.display = 'block'; // Muestra la imagen
-        }
-    </script>
+    <script src="../Js/MostMesas.js"></script>
 
-    <!-- Bootstrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-7r82R3sMKeU9DAStBbXzTc98O/YMNZ4eF9NLMb13K3uqo/W9Y5Hk4HaeQOG99UZ3" crossorigin="anonymous"></script>
 </body>
+
 </html>
-<?php
-    } 
-?>
