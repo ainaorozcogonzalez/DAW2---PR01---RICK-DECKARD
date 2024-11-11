8bee10ed3ec8682e212bc0bb933baca3bed79d18
pol-mrk <polmarc.monro@gmail.com>
2024-11-11 19:59:14 +0100

    cambios_validaciones_manager(2)

--- Manager/manager_home.php
+++ Manager/manager_home.php
@@ -23,7 +23,7 @@
 
         header('Location: ' . '../Camarero/camarero_home.php');
         exit();
-        
+
     }
 ?>
 
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manager</title>
+    <title>Camarero</title>
     <link rel="stylesheet" href="../CSS/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.0/mdb.min.css" rel="stylesheet">
 </head>
