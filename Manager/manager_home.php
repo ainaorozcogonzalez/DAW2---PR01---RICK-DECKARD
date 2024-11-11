771a96c9f66d26d1078139054ee94512a976a561
pol-mrk <polmarc.monro@gmail.com>
2024-11-11 19:59:44 +0100

    cambios_validaciones_manager(3)

--- Manager/manager_home.php
+++ Manager/manager_home.php
@@ -33,7 +33,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Camarero</title>
+    <title>Manager</title>
     <link rel="stylesheet" href="../CSS/styles.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.0.0/mdb.min.css" rel="stylesheet">
 </head>
